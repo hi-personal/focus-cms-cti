7d5093a4d117d0ba44730dda51f75d73258ac481
istvan1243 <33933467+istvan1243@users.noreply.github.com>
2026-02-26 16:57:48 +0100

    null

--- src/Installer.php
+++ src/Installer.php
@@ -2,111 +2,47 @@
 
 namespace FocusCMS\ComposerThemeInstaller;
 
-use Composer\Composer;
-use Composer\IO\IOInterface;
 use Composer\Installer\LibraryInstaller;
 use Composer\Package\PackageInterface;
-use Composer\Installer\PackageEvent;
-use Symfony\Component\Process\Process;
 
 class Installer extends LibraryInstaller
 {
-    public function getInstallPath(PackageInterface $package)
+    /**
+     * Telepítési útvonal
+     */
+    public function getInstallPath(PackageInterface $package): string
     {
-        $themeName = $this->getThemeName($package);
+        $themeName = self::getThemeNameForPackage($package);
+
         return "Themes/{$themeName}";
     }
 
-    public function supports($packageType)
+    /**
+     * Csak focus-theme csomagokat kezeli
+     */
+    public function supports($packageType): bool
     {
         return $packageType === 'focus-theme';
     }
 
-    public static function postPackageInstall(PackageEvent $event)
-    {
-        $package = $event->getOperation()->getPackage();
-        if ($package->getType() === 'focus-theme') {
-            $themeName = self::getThemeNameForPackage($package);
-            self::executeArtisanCommand($event->getIO(), "theme:setup {$themeName}");
-        }
-    }
+    /**
+     * Theme név meghatározása
+     *
+     * vendor/package-name → PackageName
+     */
+    public static function getThemeNameForPackage(
+        PackageInterface $package
+    ): string {
 
-    public static function postPackageUpdate(PackageEvent $event)
-    {
-        $operation = $event->getOperation();
+        $packageName = $package->getPrettyName();
 
-        if ($operation instanceof \Composer\DependencyResolver\Operation\UpdateOperation) {
-            $package = $operation->getTargetPackage();
-        } elseif ($operation instanceof \Composer\DependencyResolver\Operation\InstallOperation) {
-            $package = $operation->getPackage();
-        } else {
-            return;
+        if (str_contains($packageName, '/')) {
+            $packageName = explode('/', $packageName)[1];
         }
 
-        if ($package->getType() === 'focus-theme') {
-            $themeName = self::getThemeNameForPackage($package);
-            self::executeArtisanCommand($event->getIO(), "theme:setup {$themeName}");
-        }
-    }
-
-    public static function postPackageUninstall(PackageEvent $event)
-    {
-        $package = $event->getOperation()->getPackage();
-        if ($package->getType() === 'focus-theme') {
-            $themeName = self::getThemeNameForPackage($package);
-            self::executeArtisanCommand($event->getIO(), "theme:remove {$themeName}", true);
-        }
-    }
-
-    public static function getThemeNameForPackage(PackageInterface $package)
-    {
-        // mindig csak a package nevet használjuk, vendor nélkül
-        $packageName = explode('/', $package->getPrettyName())[1];
-
-        // kebab-case → PascalCase
         $themeName = str_replace('-', ' ', $packageName);
         $themeName = ucwords($themeName);
 
         return str_replace(' ', '', $themeName);
-    }
-
-    public static function executeArtisanCommand(IOInterface $io, $command, $ignoreErrors = false)
-    {
-        $cwd = getcwd();
-        $artisanPath = $cwd . '/artisan';
-
-        if (!file_exists($artisanPath)) {
-            $io->writeError("<error>Artisan fájl nem található: {$artisanPath}</error>");
-            return false;
-        }
-
-        // Ellenőrizzük, hogy létezik-e az autoload.php
-        if (!file_exists($cwd . '/vendor/autoload.php')) {
-            $io->write("<comment>Autoloader még nem létezik, kihagyjuk a parancsot</comment>");
-            return true;
-        }
-
-        $process = new Process(['php', $artisanPath, ...explode(' ', $command)]);
-        $process->setTimeout(300);
-        $process->setWorkingDirectory($cwd);
-
-        try {
-            $io->write("<comment>Végrehajtás: php artisan {$command}</comment>");
-            $process->mustRun(function ($type, $buffer) use ($io) {
-                $io->write($buffer);
-            });
-            return true;
-        } catch (\Exception $e) {
-            if (!$ignoreErrors) {
-                $io->writeError("<error>Hiba: {$e->getMessage()}</error>");
-            }
-            return false;
-        }
-    }
-
-
-    protected function getThemeName(PackageInterface $package)
-    {
-        return self::getThemeNameForPackage($package);
     }
 }
